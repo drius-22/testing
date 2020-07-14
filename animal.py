0eb876ec93e69b5ae3bb56765ecc8a908ca15707
Jorge Pena <jorge@gsbp.ca>
2020-07-14 10:48:38 -0500

    substantial change s

--- animal.py
+++ animal.py
@@ -2,12 +2,17 @@ import  sys
 
 #comment 
 
+def cat():
+    print ("meow!")
 
 def default():
     print('What is up ????')
 
 def main():
-    default()
+    if sys.argv[1] == 'cat':
+        cat()
+    else :
+        default()
 
 if __name__ == '__main__':
     main()
